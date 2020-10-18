0b35cbf5ee79509ad67c89462045274eba3cd077
Mike Ball <mikedball@gmail.com>
2020-10-18 18:26:51 -0400

    add batch_changes_test.go scaffolding

---
+++ src/batch_changes_test.go
@@ -0,0 +1,59 @@
+// +build integration
+
+/*
+Copyright 2020 Comcast Cable Communications Management, LLC
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package main
+
+import (
+	"os/exec"
+
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+	"github.com/onsi/gomega/gbytes"
+	"github.com/onsi/gomega/gexec"
+)
+
+var _ = Describe("its commands for working with batch changes", func() {
+	var (
+		session *gexec.Session
+		err     error
+	)
+
+	Describe("its 'batch-changes' command", func() {
+		Context("when it's passed '--help'", func() {
+			BeforeEach(func() {
+				args := []string{
+					"batch-changes",
+					"--help",
+				}
+
+				cmd := exec.Command(exe, args...)
+				session, err = gexec.Start(cmd, GinkgoWriter, GinkgoWriter)
+			})
+
+			AfterEach(func() {
+				if session != nil {
+					session.Terminate()
+				}
+			})
+
+			It("does not error", func() {
+				Expect(err).NotTo(HaveOccurred())
+			})
+
+			It("prints a useful description", func() {
+				Eventually(session.Out, 5).Should(gbytes.Say("List all batch changes"))
+			})
+		})
+	})
+})
